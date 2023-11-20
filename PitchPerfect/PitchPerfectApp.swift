c08d8e90dcc288043e0bf9676477c9019d9f6fbe
Pedro Basualdo <pedbasua@amazon.com>
2023-11-20 13:13:30 -0500

    Initial Commit

---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Basualdo, Pedro [C] on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
